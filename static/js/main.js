019fce716e254f52604f8f3241850356fd69cb16
Ricardo Romero <gomez.ricardo.romero@gmail.com>
2022-08-16 16:24:16 -0300

    js primeros pasos

---
+++ static/js/main.js
@@ -0,0 +1,25 @@
+let nombre = "Ricardo"  //para la creacion de variables usaremos let
+const APELLIDO = "Romero" // para las constantes usamos const, usualmente el nombre es todo en mayuscula
+
+//operaciones basicas
+let num1 = 30
+let num2 = 4
+let resu = num1 + num2
+
+//mantener comentados para no interrumpir, solo descomentar para pruebas 
+//let dato = prompt("Ingrese dato") // cuadro de dialogo que captura lo que ingresa el usuario
+//alert("este es un mensaje de alert") // cuadro de dialogo que muestra mensaje
+//let confirmar = confirm("estas seguro?")//cuadro de dialogo mensaje + boton
+
+//imprimir mensajes se usan para hacer pruebas, mantener comentados 
+//console.log("tu nombre es "+ nombre + " y tu apellido es "+ APELLIDO)
+//console.warn("tu nombre es "+ nombre + " y tu apellido es "+ APELLIDO)
+//console.error("tu nombre es "+ nombre + " y tu apellido es "+ APELLIDO)
+
+//ejercicio en clase
+/* let dato1 = prompt("ingrese nombre")
+let dato2 = prompt("ingrese apellido")
+let nombrecompleto = ("su nombre completo es "+ dato1 + " " + dato2)
+console.log(nombrecompleto)
+console.warn(nombrecompleto)
+console.error(nombrecompleto) */
