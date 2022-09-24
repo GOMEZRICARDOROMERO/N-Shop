c1e1282949049514a5dd0a4765207049b928875a
Ricardo Romero <gomez.ricardo.romero@gmail.com>
2022-09-24 19:29:56 -0300

    Segunda-entrega-proyecto-final

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,3 +1,37 @@
+//array principal de objetos
+let arrayitems =[] //uso let para poder igualarlo mas adelante
+let carrito =[]
+
+//id para los productos
+const id = ()=> parseInt(Math.random() * 100000) //creo un ID numérico dinámico
+
+//constructor
+class Items {
+    constructor(id,nombre, stock) {
+        this. id = id
+        this.nombre = nombre
+        this.stock = stock
+    }
+}
+
+//llenado automatico de productos
+function autoProductos (){
+    arrayitems.push(new Items(id(),"Cable hdmi",10))
+    arrayitems.push(new Items(id(),"Mando pro",30))
+    arrayitems.push(new Items(id(),"Zelda breath of the wild",2))
+    arrayitems.push(new Items(id(),"Mario kart 8",8))
+    arrayitems.push(new Items(id(),"Base dock",1))
+    arrayitems.push(new Items(id(),"Base mando",5))
+    arrayitems.push(new Items(id(),"Adaptador mando gamecube",2))
+    arrayitems.push(new Items(id(),"Mando gamecube",6))
+    arrayitems.push(new Items(id(),"Estuche nintendo switch",4))
+    arrayitems.push(new Items(id(),"Protector pantalla",7))
+    arrayitems.push(new Items(id(),"Cubre sticks mandos",2))
+    arrayitems.push(new Items(id(),"Xenoblade chronicles 3",15))
+}
+
+autoProductos()
+
 //me conecto con los elemtnos de test.html lineas 38 y 39
 const titulo = document.getElementById("titulo") // me conecto con el elemento con id titulo 
 const parrafo = document.getElementById("parrafo") // me conecto con el elemento con id parrafo
@@ -6,6 +40,7 @@ const tabla = document.getElementById("tabla")
 //me cnecto a los botones
 const agregarItems = document.querySelector("#aitems")
 const borrarItems = document.querySelector("#bitems")
+const btncarrito = document.querySelector("#carrito")
 
 //me conecto al buscar
 const buscarItem = document.querySelector("#buscaritem")
@@ -19,12 +54,23 @@ function filtrarProductos() { //FILTRAR PRODUCTOS EN LA TABLA INGRESANDO PARTE D
                 console.clear()
                 console.warn("No se encontraron productos.")
                 agregarProductosAlHtml(arrayitems)
+                accionButtonAdd(arrayitems) //llamo de vuelta para que funcione el boton 
             } else {
                 agregarProductosAlHtml(resultado)
+                accionButtonAdd(resultado) //llamo de vuelta apra que funcione en el filtro pero pasandole el nuevo array
             }
     } else {
         agregarProductosAlHtml(arrayitems)
+        accionButtonAdd(arrayitems)
     }
+}
+
+inputFiltrar.addEventListener("input",filtrarProductos) //A medida que escribimos.
+
+//paso a mayus la primera letra
+function pasarAmayusLaPrimera(str){
+    cadena = str.toLowerCase()//primero paso todo a minuscula
+    return cadena = cadena.charAt(0).toUpperCase() + cadena.slice(1)//tomo la primera letra y la paso a mayus y le agrego el resto de la cadena
 }
 
 
@@ -36,11 +82,68 @@ function agregarProductosAlHtml(array){
                     <td>${item.id}</td>
                     <td>${item.nombre}</td>
                     <td>${item.stock}</td>
+                    <td><button class="btn btn-primary" id="btn${item.id}">Add 🛒</button></td>
                 </tr>`
     tabla.innerHTML +=fila
     });
 }
+
 agregarProductosAlHtml(arrayitems)
+
+function agregarProducto(){
+    let nombre = prompt("Ingresa el nombre del producto:") //pide dato al usuario
+    nombre = pasarAmayusLaPrimera(nombre) // paso a mayuscula la primer aletra
+    let stock = parseInt(prompt("Ingresa el stock:"))
+    const newProd = {id: id(), nombre: nombre, stock: stock} //crea un objeto literal con los datos
+    arrayitems.push(newProd)//agrego el objeto al array
+    console.warn("Array con objeto agregado")
+    agregarProductosAlHtml(arrayitems)
+    alert("Se agrego el producto correctamente")
+    accionButtonAdd (arrayitems) // lo llamo de nuevo para que el nuevo item funcione
+}
+
 agregarItems.addEventListener("click",agregarProducto)
+
+function quitarProducto(){
+    let id = parseInt(prompt("Ingrese el id del producto")) //tomo el id 
+    let copiaArray = arrayitems.filter((item) => item.id !==id) //busco el id en el array y hago una copia sin el objeto
+    if(copiaArray.length === arrayitems.length){ //si la longitud de la copia es igual al original significa que no encontro el id o que no existe
+        alert("Error ingrese un id valido")
+        quitarProducto()
+    }else{
+        console.warn("Array con objeto retirado")
+        arrayitems = copiaArray //igualo el array original a la copia 
+        agregarProductosAlHtml(arrayitems)
+        alert("Se quito el producto correctamente")
+        accionButtonAdd (arrayitems)
+    }
+}
+
 borrarItems.addEventListener("click",quitarProducto)
-inputFiltrar.addEventListener("input", filtrarProductos) //A medida que escribimos.
+
+//funcion para conectarse con cada boton y generar su accion
+function accionButtonAdd (array) {
+    array.forEach(item => { //recorro el aaray
+        const boton = document.querySelector(`#btn${item.id}`) // por cada item genero un boton con un id
+        boton.addEventListener("click",()=>agregarItemAlCarrito(`${item.id}`)) // cada boton llama la funcion de carrito y le pasa el id
+    })
+}
+
+accionButtonAdd (arrayitems) //llamo para que se inicie 
+
+//funcion para agregar el producto al carrito por id
+function agregarItemAlCarrito(id) {
+    const item = arrayitems.find(item => item.id == id)//busco el item del array y lo guardo en la const item
+        carrito.push(item)//agrego ese item al array de carryto
+        localStorage.setItem("carrito", JSON.stringify(carrito)) //agrego al localstorage
+        alert("Se Agrego "+`${item.nombre}`+" al carrito 🛒")
+}
+
+//funcion cargar carrito trae los datos cargados esto se usa para no perder los que se agregaron antes
+function cargarCarrito() {
+    if (localStorage.getItem("carrito")) {
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+    }
+}
+
+cargarCarrito()
---
+++ static/js/domcarrito.js
@@ -0,0 +1,55 @@
+//array
+let carrito = []
+carrito = JSON.parse(localStorage.getItem("carrito")) //traigo los items del storage
+let carritovacio = []
+let copiaArray =[]
+
+
+//me conecto
+const tabla = document.querySelector("#tablacarrito")
+const limpiar = document.querySelector("#bcarrito")
+
+
+//carga el carrito y lo muestra 
+function cargarCarrito(array){
+    let fila = " "
+    tabla.innerHTML = fila /* primero limpia la anterior */
+    array.forEach(item => {
+        fila = `<tr>
+                    <td>${item.id}</td>
+                    <td>${item.nombre}</td>
+                    <td><button class="btn btn-primary" id="btn${item.id}">Quitar</button></td>
+                </tr>`
+    tabla.innerHTML +=fila
+    });
+}
+
+//llamo la funcion para que se cargue
+cargarCarrito(carrito)
+
+//cuando se presiona le boton borra todo el storage y llama de nuevo la funcion cargar carrito para actualizar
+limpiar.addEventListener("click", ()=>{
+    localStorage.clear()
+    cargarCarrito(carritovacio)//cargo un array vacio para que no me de error
+    alert("Se limpio el carrito")
+})
+
+//funcion para conectarse con cada boton y generar su accion
+function accionButtonQuitar (array) {
+    array.forEach(item => { //recorro el aaray
+        const boton = document.querySelector(`#btn${item.id}`) // por cada item genero un boton con un id
+        boton.addEventListener("click",()=>quitar(`${item.id}`)) 
+    })
+}
+
+accionButtonQuitar(carrito)
+
+
+function quitar(id){
+    let idp = parseInt(id)
+    copiaArray = carrito.filter((item) => item.id !==idp) //busco el id en el array y hago una copia sin el objeto
+        carrito = copiaArray //igualo el carrito
+        localStorage.setItem("carrito", JSON.stringify(carrito)) //reemplazo el storage 
+        cargarCarrito(carrito) //cargo de nuevo los datos en el html
+        accionButtonQuitar(carrito) // acctivo nuevamente los botones
+}
